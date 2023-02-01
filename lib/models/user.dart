5ce835ad99200cae7a19e486db4a8ed1cf72e1d9
Ali <a.aljumah2000@gmail.com>
2023-02-01 23:51:07 +0300

    just to try

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class User {
+class User with ChangeNotifier {
 
   late final String   uid;
   late final String   fname;
@@ -10,7 +11,22 @@ class User {
   late final String?  photoUrl; 
   late final String?  token;
 
-  User({required this.uid, required this.fname, required this.lname, required this.email, this.phone, this.photoUrl, this.token});
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+
+  // User({required this.uid, required this.fname, required this.lname, required this.email, this.phone, this.photoUrl, this.token});
+
+  Future getUserInfo(String uid) async {
+    
+    final ref = db.collection('users').doc(uid);
+    
+    ref.get().then(
+      (DocumentSnapshot doc) {
+        final data = doc.data() as Map<String, dynamic>;
+        print(data['name']);
+      },
+      onError: (e) => print("Error getting document: $e"),
+    );
+  }
 
 
 
--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,11 +1,11 @@
-// import 'dart:html';
-
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:careergy_mobile/models/user.dart' as usr;
 import 'package:flutter/material.dart';
 
 class AuthProvider with ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore db = FirebaseFirestore.instance;
   late final usr.User user;
 
   bool get isAuth {
@@ -17,8 +17,9 @@ class AuthProvider with ChangeNotifier {
     try {
       final credential = await FirebaseAuth.instance
           .signInWithEmailAndPassword(email: emailAddress, password: password);
-      // user = usr.User(uid: );
-      print( credential.user!.uid);
+      user = usr.User();
+      final data = user.getUserInfo(credential.user!.uid);
+      print(data);
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
         print('No user found for that email.');
@@ -34,9 +35,9 @@ class AuthProvider with ChangeNotifier {
     try {
       final credential =
           await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: emailAddress,
-        password: password,
-      );
+            email: emailAddress,
+            password: password,
+          );
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
         print('The password provided is too weak.');
