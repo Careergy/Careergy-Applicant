6a8e86dde24d3adfefc9300b16959fdc7f73b0e6
7rkl <m7mmad-111@hotmail.com>
2023-02-01 17:41:31 +0300

    objectMessage

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,19 +1,16 @@
-import 'dart:html';
+// import 'dart:html';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:careergy_mobile/models/user.dart' as usr;
 
 class AuthProvider {
-
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   //sign in with email & password
   Future login(String emailAddress, String password) async {
     try {
-      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: emailAddress,
-        password: password
-      );
+      final credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: emailAddress, password: password);
       print(credential);
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
@@ -25,23 +22,24 @@ class AuthProvider {
   }
 
   //register
-Future signup(String emailAddress, String password, String phone, String name) async {
-  try {
-  final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-    email: emailAddress,
-    password: password,
-  );
-} on FirebaseAuthException catch (e) {
-  if (e.code == 'weak-password') {
-    print('The password provided is too weak.');
-  } else if (e.code == 'email-already-in-use') {
-    print('The account already exists for that email.');
+  Future signup(
+      String emailAddress, String password, String phone, String name) async {
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
   }
-} catch (e) {
-  print(e);
-}
-}
 
   //log out
-
 }
