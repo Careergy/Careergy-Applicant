b3183db0d31fe1cbecff80aad806be59085b018e
Sulaiman <Silly9_@hotmail.com>
2023-02-20 02:45:17 +0300

    commit company profile screen

---
+++ lib/screens/company_profile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class CompanyProfile extends StatefulWidget {
+  const CompanyProfile({super.key});
+
+  @override
+  State<CompanyProfile> createState() => _CompanyProfileState();
+}
+
+class _CompanyProfileState extends State<CompanyProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
